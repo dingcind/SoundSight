dcdf1572518d73cf07fbf44c9368c4e95e99380a
Cindy Ding <cindy.ding@mail.utoronto.ca>
2018-01-20 11:34:12 -0500

    CameraSetup

--- iOS/SoundSight/CameraControl/CameraViewController.swift
+++ iOS/SoundSight/CameraControl/CameraViewController.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+import UIkit
+import AVFoundation
+
+class CameraViewController: UIViewController{
+
+  let session = AVCaptureSession()
+  var camera : AVCaptureDevice?
+  var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
+  var cameraCaptureOutput : AVCapturePhotoOutput?
+
+
+  override func viewDidLoad(){
+    super.viewDidLoad()
+    initializeCaptureSession()
+    // additional setup
+  }
+
+  func initializeCaptureSession(){
+    session.sessionPreset = AVCaptureSessionPresetHigh
+    camera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+
+    do{
+      let cameraCaptureInput = try AVCaptureDeviceInput(device: camera!)
+      cameraCaptureOutput = AVCapturePhotoOutput()
+
+      session.addInput(cameraCaptureInput)
+      session.addOutput(cameraCaptureOutput)
+
+    } catch {
+      print(error.localizedDescription)
+    }
+
+    cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
+    cameraPreviewLayer?.videoGravity = AVLayerVideoGravityAspectFill
+    cameraPreviewLayer?.frame = view.bounds
+    cameraPreviewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+
+    view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+
+    session.startRunning()
+
+  }
+
+  override func didReceiveMemoryWarning(){
+    super.didReceiveMemoryWarning()
+
+  }
+}
