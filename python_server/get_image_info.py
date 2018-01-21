bada78cb97e1ed3533b15f5d76e4487d82f71dbf
Anna Verkhovskaya <verkhovskaya.anna@gmail.com>
2018-01-20 21:30:29 -0500

    other server.py

--- python_server/get_image_info.py
+++ python_server/get_image_info.py
@@ -15,36 +15,74 @@ subscription_key = '0c723cbc14cf4d0abe07932a39484b0b'
 #
 # NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
 # a free trial subscription key, you should not need to change this region.
-uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
-# Request headers.
-headers = {
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': subscription_key,
-}
+def get_info():
+	uri_base = 'westcentralus.api.cognitive.microsoft.com'
 
-# Request parameters.
-params = urllib.urlencode({
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,facialHair,glasses,hair,makeup,accessories',
-})
+	# Request headers.
+	headers = {
+		'Content-Type': 'application/json',
+		'Ocp-Apim-Subscription-Key': subscription_key,
+	}
 
-# The URL of a JPEG image to analyze.
-body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+	# Request parameters.
+	params = urllib.urlencode({
+		'returnFaceId': 'true',
+		'returnFaceLandmarks': 'false',
+		'returnFaceAttributes': 'age,gender,facialHair,glasses,hair,makeup,accessories',
+	})
 
-try:
-    # Execute the REST API call and get the response.
-    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
+	# The URL of a JPEG image to analyze.
+	body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
 
-    # 'data' contains the JSON data. The following formats the JSON data for display.
-    parsed = json.loads(data)
-    print ("Response:")
-    print (json.dumps(parsed, sort_keys=True, indent=2))
-    conn.close()
+	try:
+		# Execute the REST API call and get the response.
+		conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+		conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+		response = conn.getresponse()
+		data = response.read()
 
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+		# 'data' contains the JSON data. The following formats the JSON data for display.
+		parsed = json.loads(data)
+		print ("Response:")
+		print (json.dumps(parsed, sort_keys=True, indent=2))
+		conn.close()
+
+	except Exception as e:
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def create_group():
+	def get_info():
+		uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+		# Request headers.
+		headers = {
+			'Content-Type': 'application/json',
+			'Ocp-Apim-Subscription-Key': subscription_key,
+		}
+
+		# Request parameters.
+		params = urllib.urlencode({
+			'returnFaceId': 'true',
+			'returnFaceLandmarks': 'false',
+			'returnFaceAttributes': 'age,gender,facialHair,glasses,hair,makeup,accessories',
+		})
+
+		# The URL of a JPEG image to analyze.
+		body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg'}"
+
+		try:
+			# Execute the REST API call and get the response.
+			conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+			conn.request("POST", "/face/v1.0/detect?%s" % params, body, headers)
+			response = conn.getresponse()
+			data = response.read()
+
+			# 'data' contains the JSON data. The following formats the JSON data for display.
+			parsed = json.loads(data)
+			print("Response:")
+			print(json.dumps(parsed, sort_keys=True, indent=2))
+			conn.close()
+
+		except Exception as e:
+			print("[Errno {0}] {1}".format(e.errno, e.strerror))
