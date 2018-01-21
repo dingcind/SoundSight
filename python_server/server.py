8198d848797c43995689a196aba99a53aef2e2df
Anna Verkhovskaya <verkhovskaya.anna@gmail.com>
2018-01-20 20:06:47 -0500

    Quin's updates

--- python_server/server.py
+++ python_server/server.py
@@ -6,8 +6,11 @@ import os
 import math
 import sys
 import cv2
+import base64
+from io import StringIO
+import json
 
-new_image_path = "img.png"
+new_image_path = "img.jpg"
 resulting_data_path = "result.txt"
 
 os.chdir("../yolo-9000/darknet/python")
@@ -19,6 +22,7 @@ os.chdir("..")
 net = load_net("cfg/yolo9000.cfg", "../yolo9000-weights/yolo9000.weights", 0)
 meta = load_meta("cfg/combine9k.data")
 #r = detect(net, meta, "data/dog.jpg")
+os.chdir("../../python_server")
 
 class Item:
 	def __init__(self, name, pos, size):
@@ -49,7 +53,7 @@ items_global = []
 def normalize(new_data):
 	global items_global
 	for each in new_data:
-		for item in items_gxlobal:
+		for item in items_global:
 			if abs(item.pos-each["pos"]) < 0.1 and abs(item.size-each["size"]) < 0.1:
 				item.move(each["pos"], each["size"])
 				break
@@ -66,15 +70,23 @@ def normalize(new_data):
 @route('/main', method='POST')
 def main():
 	new_image = open(new_image_path, "wb")
-	img_data = request.data.read()
-	new_image.write(img_data.decode)
+        #print(request.body.read())
+        #print(dir(request.body.read()))
+	img_data = request.body.read()
+        img_data = img_data.decode()
+        i = StringIO(img_data)
+        dit = json.load(i)
+        text = dit["img"]
+        text = text.encode("ascii")
+	new_image.write(base64.decodestring(text))
 	new_image.close()
 	r = detect(net, meta, new_image_path)
 	im = cv2.imread(new_image_path)
-	width, height = im.size
+        #print(im.shape)
+	width, height = im.shape[0:2]
         #im = cv2.rectangle(im, (), (), (255,0,0,), 7)
 	#os.system("rm -rf " + new_image_path)
-
+        print(r)
 	objects = []
 	for obj in r:
 		name = obj[0]
@@ -83,11 +95,17 @@ def main():
 		objects.append({"name":name, "pos":pos, "size":size})
                 im = cv2.rectangle(im, (obj[2][0], obj[2][1]), (obj[2][2], obj[2][3]), (255, 0, 0), 7)
         cv2.imwrite("test.png", im)
+        print("*********")
+        print("Before Normalization")
 	objects = normalize(objects)
-	return {"data": normalize(objects)}  # NOTE: For security reasons, you CANNOT return a top level array. Send it as {"data":[array]}
+        print("After Normalization")
+        print(objects)
+        print("************\n**************")
+	return {"data": objects}  # NOTE: For security reasons, you CANNOT return a top level array. Send it as {"data":[array]}
 
 @route('/get_example')
 def get_example():
 	return {"data": [{"pos": -0.3, "size": 0.3, "name": "Quin"}, {"pos": 0.7, "size": 0.6, "name": "Anna"}]}
 
 run(host='0.0.0.0', port=80, debug=True, reloader=True)
+
